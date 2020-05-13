3d70e613558a6f4397f50873c916e02fedaf871a
Extro <22ff2296@gmail.com>
2020-05-13 09:31:00 +0500

    Добавьте файлы проекта.

---
+++ serverChat/ChatController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverChat
+{
+    public static class ChatController
+    {
+        private const int _maxMessage = 100;
+        public static List<message> Chat = new List<message>();
+        public struct message
+        {
+            public string userName;
+            public string data;
+            public message(string name, string msg)
+            {
+                userName = name;
+                data = msg;
+            }
+        }
+        public static void AddMessage(string userName,string msg) // добовление сообщений
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(msg)) return;
+                int countMessages = Chat.Count;
+                if (countMessages > _maxMessage) ClearChat();
+                message newMessage = new message(userName, msg);
+                Chat.Add(newMessage);
+                Console.WriteLine("Новое сообщение от {0}.",userName);
+                Server.UpdateAllChats();
+            }
+            catch (Exception exp) { Console.WriteLine("Ошибка сообщения: {0}.", exp.Message); }
+        }
+        public static void ClearChat()  // Удаление сообщений
+        {
+            Chat.Clear();
+        }
+        public static string GetChat() 
+        {
+            try
+            {
+                string data = "#updatechat&";
+                int countMessages = Chat.Count;
+                if (countMessages <= 0) return string.Empty;
+                for (int i = 0; i < countMessages; i++)
+                {
+                    data += String.Format("{0}~{1}|", Chat[i].userName, Chat[i].data);
+                }
+                return data;
+            }
+            catch (Exception exp) { Console.WriteLine("Ошибка обновления: {0}", exp.Message); return string.Empty; }
+        }
+    }
+}
---
+++ serverChat/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace serverChat
+{
+    public static class Server
+    {
+        public static List<Client> Clients = new List<Client>();
+        public static void NewClient(Socket handle)  // создание нового пользовотеля
+        {
+            try
+            {
+                Client newClient = new Client(handle);
+                Clients.Add(newClient);
+                Console.WriteLine("Новый клиент подключен: {0}", handle.RemoteEndPoint);
+            }
+            catch (Exception exp) { Console.WriteLine("Ошибка с добавлением пользовотеля: {0}.",exp.Message); }
+        }
+        public static void EndClient(Client client)  // Выход пользовотеля
+        {
+            try
+            {
+                client.End();
+                Clients.Remove(client);
+                Console.WriteLine("Пользовотель {0} отключился.", client.UserName);
+            }
+            catch (Exception exp) { Console.WriteLine("Ошибка отключения: {0}.",exp.Message); }
+        }
+        public static void UpdateAllChats()  // Полное обновление кол пользовотелей
+        {
+            try
+            {
+                int countUsers = Clients.Count;
+                for (int i = 0; i < countUsers; i++)
+                {
+                    Clients[i].UpdateChat();
+                }
+            }
+            catch (Exception exp) { Console.WriteLine("Ошибка обновления: {0}.",exp.Message); }
+        }
+        
+    }
+}
